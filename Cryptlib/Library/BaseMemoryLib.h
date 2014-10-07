4960f3580e74352a37437801c2e633ff36b84943
Steve Langasek <steve.langasek@canonical.com>
2014-10-07 16:20:02 -0700

    Update debian/patches/prototypes with some new declarations needed for openssl 0.9.8za update.

--- Cryptlib/Library/BaseMemoryLib.h
+++ Cryptlib/Library/BaseMemoryLib.h
@@ -8,4 +8,34 @@ ScanMem8 (
     IN CHAR8           Value
     );
 
+UINT32
+WriteUnaligned32(
+    UINT32            *Buffer,
+    UINT32             Value
+    );
+
+CHAR8 *
+AsciiStrCat(
+    CHAR8             *Destination,
+    CHAR8             *Source
+    );
+
+CHAR8 *
+AsciiStrCpy(
+    CHAR8             *Destination,
+    CHAR8             *Source
+    );
+
+CHAR8 *
+AsciiStrnCpy(
+    CHAR8             *Destination,
+    CHAR8             *Source,
+    UINTN              count
+    );
+
+UINTN
+AsciiStrSize(
+    CHAR8             *string
+    );
+
 #endif
